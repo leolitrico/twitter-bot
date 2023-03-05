d7d0c3cf55e0c3aa8d0c52187c9bc076962f04c1
litrico <leo.litrico@epfl.ch>
2023-03-05 12:25:56 +0100

    changed programme architecture, now we have two separate scripts, one that scraps users that have a high probability of following back (done). And one where the user actually follows and unfollows (in progress)

---
+++ bot/usersToFollow.py
@@ -0,0 +1,114 @@
+# Description: This script will find users that are likely to follow a target user back
+# Arguments:
+    # botUsername: username of the bot account
+    # botPassword: password of the bot account
+    # targetUsername: username of the target account
+# Options:
+    # --profile: use a chrome profile for the bot (--profile path profileName)
+    # --follower-limit: max number of followers an eligible user can have to follow them
+    # --ratio-limit: limit the ratio of followers to following an eligible user can have to follow them
+    # --file: file to store the users in
+
+
+import sys
+import pathlib
+sys.path.append(str(pathlib.Path(__file__).parent.absolute()) + "/../")
+
+from libs.twitter import login, get_followers, get_follow_count, get_following
+from libs.browser import get_browser
+
+##############################################################################################
+# constants
+##############################################################################################
+PROFILE_OPT = "--profile"
+
+FOLLOWER_LIMIT_OPT = "--follower-limit"
+FOLLOWER_LIMIT = 30
+
+RATIO_LIMIT_OPT = "--ratio-limit"
+RATIO_LIMIT = 3
+
+FILE_OPT = "--file"
+
+NUMBER_TRIES = 100
+
+def main():
+    ##############################################################################################
+    # parse arguments
+    ##############################################################################################
+    botUsername = None
+    botPassword = None
+    targetUsername = None
+    profile_path = None
+    botProfile = None
+    filename = None
+    followerLimit = FOLLOWER_LIMIT
+    ratioLimit = RATIO_LIMIT
+    try:
+        argv = sys.argv[1:]
+
+        botUsername = argv[0]
+        botPassword = argv[1]
+
+        targetUsername = argv[2]
+
+        if PROFILE_OPT in argv:
+            i = argv.index(PROFILE_OPT)
+            profile_path = argv[i + 1]
+            botProfile = argv[i + 2]
+
+        if FOLLOWER_LIMIT_OPT in argv:
+            i = argv.index(FOLLOWER_LIMIT_OPT)
+            followerLimit = int(argv[i + 1])
+
+        if RATIO_LIMIT_OPT in argv:
+            i = argv.index(RATIO_LIMIT_OPT)
+            ratioLimit = int(argv[i + 1])
+        
+        if FILE_OPT in argv:
+            i = argv.index(FILE_OPT)
+            filename = argv[i + 1]
+    except:
+        print("Error: invalid arguments")
+        return
+
+    ##############################################################################################
+    # main
+    ##############################################################################################
+
+    botBrowser = get_browser(profile_path, botProfile)
+
+    #login to bot account
+    login(botBrowser, botUsername, botPassword)
+
+    #get all my followers
+    targetFollowers = get_followers(botBrowser, targetUsername, numberOfTries=NUMBER_TRIES)
+    targetFollowing = get_following(botBrowser, targetUsername, numberOfTries=NUMBER_TRIES)
+    #init list of new people to follow
+    usersToFollow = []
+
+    #file to store all the users to follow
+    file = None
+    if filename == None:
+        file = sys.stdout
+    else:
+        try: 
+            file = open(filename, "w")
+        except:
+            print("Error: unable to write to file " + filename)
+            return
+
+    #for each of target's followers, get their followers
+    for follower in targetFollowers:
+        theirFollowers = get_followers(botBrowser, follower, limit=1000, numberOfTries=NUMBER_TRIES)
+        for f in theirFollowers:
+            #if: they are me, or they are the bot, or they are already in my list, or they are already in my followers, skip
+            if not f in targetFollowing and f != targetUsername and f != botUsername and f not in usersToFollow and not f in targetFollowers:
+                following, followers = get_follow_count(botBrowser, f, numberOfTries=NUMBER_TRIES)
+                #check that they have a high probability of following back
+                if following / followers > ratioLimit and followers < followerLimit:
+                    usersToFollow.append(f)
+                    file.write(f + "\n")
+    file.close()
+
+
--- libs/twitter.py
+++ libs/twitter.py
@@ -10,78 +10,135 @@ def random_sleep(min):
     sleep(random.randint(min, min + RANGE))
 
 
-def login(browser, username, password):
-    browser.get("https://twitter.com/home")
-    random_sleep(4)
+def login(browser, username, password, numberOfTries=1):
+    for i in range(numberOfTries):
+        try:
+            browser.get("https://twitter.com/home")
+            random_sleep(4)
 
-    if browser.current_url == "https://twitter.com/home":
-        return
-    
-    browser.get("http://twitter.com/i/flow/login/")
-    random_sleep(4)
-    #Enter Your Username Here
-    browser.find_element(By.XPATH,"//input[@name='text']").send_keys(username)
-    browser.find_element(By.XPATH,"/html/body/div[1]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]").click()
-    random_sleep(3)
-    #Enter Your Password here
-    browser.find_element(By.XPATH,"//input[@name='password']").send_keys(password)
-    browser.find_element(By.XPATH,"/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div").click()
-
-def get_followers(browser, name, limit):
-    follower_list = []
-    browser.get("https://twitter.com/" + name + "/followers")
-    random_sleep(4)
-    # Code to goto End of the Page
-    last_height = browser.execute_script("return document.body.scrollHeight")
-    counter = 0
-    while True:
-        #get usernames element
-        usernames = browser.find_elements(By.XPATH, "//div[@data-testid='cellInnerDiv']//a[@role='link']//span")
-        for username in usernames:
-            if counter >= limit:
-                return follower_list
+            if browser.current_url == "https://twitter.com/home":
+                return
             
-            username = username.text
-            if username not in follower_list and len(username) > 0 and username[0] == '@':
-                follower_list.append(username[1:])
-                print(username[1:])
-                counter += 1
-        # Scroll down to bottom
-        browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        # Wait to load page
-        random_sleep(1)
-        # Calculate new scroll height and compare with last scroll height
-        new_height = browser.execute_script("return document.body.scrollHeight")
-        if new_height == last_height:
-            break
-        last_height = new_height
-    return follower_list
+            browser.get("http://twitter.com/i/flow/login/")
+            random_sleep(4)
+            #Enter Your Username Here
+            browser.find_element(By.XPATH,"//input[@name='text']").send_keys(username)
+            browser.find_element(By.XPATH,"/html/body/div[1]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]").click()
+            random_sleep(3)
+            #Enter Your Password here
+            browser.find_element(By.XPATH,"//input[@name='password']").send_keys(password)
+            browser.find_element(By.XPATH,"/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div").click()
+        except:
+            continue
+
+def get_followers(browser, name, limit=None, numberOfTries=1):
+    for i in range(numberOfTries):
+        try:
+            follower_list = []
+            browser.get("https://twitter.com/" + name + "/followers")
+            random_sleep(4)
+            # Code to goto End of the Page
+            last_height = browser.execute_script("return document.body.scrollHeight")
+            counter = 0
+            while True:
+                #get usernames element
+                usernames = browser.find_elements(By.XPATH, "//div[@data-testid='cellInnerDiv']//a[@role='link']//span")
+                for username in usernames:
+                    if limit != None and counter >= limit:
+                        return follower_list
+                    
+                    username = username.text
+                    if username not in follower_list and len(username) > 0 and username[0] == '@':
+                        follower_list.append(username[1:])
+                        print(username[1:])
+                        counter += 1
+                # Scroll down to bottom
+                browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                # Wait to load page
+                random_sleep(1)
+                # Calculate new scroll height and compare with last scroll height
+                new_height = browser.execute_script("return document.body.scrollHeight")
+                if new_height == last_height:
+                    break
+                last_height = new_height
+            return follower_list
+        except:
+            continue
+    return []
+
+def get_following(browser, name, limit=None, numberOfTries=1):
+    for i in range(numberOfTries):
+        try:
+            follower_list = []
+            browser.get("https://twitter.com/" + name + "/following")
+            random_sleep(4)
+            # Code to goto End of the Page
+            last_height = browser.execute_script("return document.body.scrollHeight")
+            counter = 0
+            while True:
+                #get usernames element
+                usernames = browser.find_elements(By.XPATH, "//div[@data-testid='cellInnerDiv']//a[@role='link']//span")
+                for username in usernames:
+                    if limit != None and counter >= limit:
+                        return follower_list
+                    
+                    username = username.text
+                    if username not in follower_list and len(username) > 0 and username[0] == '@':
+                        follower_list.append(username[1:])
+                        print(username[1:])
+                        counter += 1
+                # Scroll down to bottom
+                browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                # Wait to load page
+                random_sleep(1)
+                # Calculate new scroll height and compare with last scroll height
+                new_height = browser.execute_script("return document.body.scrollHeight")
+                if new_height == last_height:
+                    break
+                last_height = new_height
+            return follower_list
+        except:
+            continue
+    return []
 
 #returns (following, followers)
-def get_follow_count(browser, name):
-    browser.get("https://twitter.com/" + name)
-    random_sleep(4)
-    #get follower count element
-    follower_count = browser.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div/div/div/div[5]/div[2]/a/span[1]/span")
-    #get following count element
-    following_count = browser.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div/div/div/div[5]/div[1]/a/span[1]/span")
+def get_follow_count(browser, name, numberOfTries=1):
+    for i in range(numberOfTries):
+        try:
+            browser.get("https://twitter.com/" + name)
+            random_sleep(4)
+            #get follower count element
+            follower_count = browser.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div/div/div/div[5]/div[2]/a/span[1]/span")
+            #get following count element
+            following_count = browser.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div/div/div/div[5]/div[1]/a/span[1]/span")
 
-    locale.setlocale( locale.LC_ALL, 'en_US.UTF-8' ) 
-    try:
-        return locale.atoi(following_count.text), locale.atoi(follower_count.text)
-    except:
-        return 100, 10000
+            locale.setlocale( locale.LC_ALL, 'en_US.UTF-8' ) 
+            try:
+                return locale.atoi(following_count.text), locale.atoi(follower_count.text)
+            except:
+                return 100, 10000
+        except:
+            continue
+    return 100, 10000
 
-def follow(browser, name):
-    browser.get("https://twitter.com/" + name)
-    random_sleep(4)
-    #get follow button element
-    follow_button = browser.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div/div/div/div[1]/div[2]/div[3]/div[1]/div")
-    follow_button.click()
+def follow(browser, name, numberOfTries=1):
+    for i in range(numberOfTries):
+        try:
+            browser.get("https://twitter.com/" + name)
+            random_sleep(4)
+            #get follow button element
+            follow_button = browser.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div/div/div/div[1]/div[2]/div[3]/div[1]/div")
+            follow_button.click()
+        except:
+            continue
 
-def unfollow(browser, name):
-    browser.get("https://twitter.com/" + name)
-    random_sleep(4)
-    #get follow button element
-    follow_button = browser.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div/div/div[1]/div[1]/div[2]/div[4]/div[1]/div")
-    follow_button.click()
+def unfollow(browser, name, numberOfTries=1):
+    for i in range(numberOfTries):
+        try:
+            browser.get("https://twitter.com/" + name)
+            random_sleep(4)
+            #get follow button element
+            follow_button = browser.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div/div/div[1]/div[1]/div[2]/div[4]/div[1]/div")
+            follow_button.click()
+        except:
+            continue
